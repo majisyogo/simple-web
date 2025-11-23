ced27ff4c741fc820bad46d3456639a6d041adb7
Maji <majisyogo0414@gmail.com>
2025-11-23 21:10:05 +0700

    Update and rename main.tf to maincode.tf

--- maincode.tf
+++ maincode.tf
@@ -1,17 +1,19 @@
+#Select Region
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-# 最新 Amazon Linux 2 AMI を取得
+# Get the latest AMI
 data "aws_ssm_parameter" "amazon_linux" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+#Create EC2
 resource "aws_instance" "simple_web" {
   ami           = data.aws_ssm_parameter.amazon_linux.value
-  instance_type = "t3.micro"
+  instance_type = "t3.micro"　#I can chose an instance type.
 
   tags = {
     Name = "SimpleWebServer"
-  }
+  } #I can enter any name as a tag name.
 }
